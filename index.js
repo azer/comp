6444fc102edc5adf8e30929dbe6fabbdaefdc73f
E.Azer Koçulu <azer@kodfabrik.com>
2014-02-26 18:24:22 -0800

    eleminates last step passing the final callback to the last fn

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 module.exports = comp;
 
-function comp(/* functions */){
+function comp(/* functions */) {
   var functions = Array.prototype.slice.call(arguments);
 
   call.then = then;
 
   return call;
 
-  function call(/* [firstValue,] callback */){
+  function call(/* [firstValue,] callback */) {
     var hasFirstValue = arguments.length > 1,
-        firstValue    = hasFirstValue ? arguments[0] : undefined,
-        callback      = arguments[ hasFirstValue ? 1 : 0 ],
+        firstValue = hasFirstValue ? arguments[0] : undefined,
+        callback = arguments[ hasFirstValue ? 1 : 0 ],
         params;
 
-    function next(i/* [, currentValue ] */){
+    function next(i/* [, currentValue ] */) {
       params = arguments.length > 1 ? [ arguments[1] ] : [];
 
-      params.push(function(error, newValue){
-        if(error) return callback(error);
+      if (i + 1 >= functions.length) {
+        params.push(callback);
+      } else {
+        params.push(function(error, newValue){
+          if (error) return callback(error);
+          if (i + 1 >= functions.length) return callback(undefined, newValue);
 
-        if( i + 1 >= functions.length ) return callback(undefined, newValue);
-        next( i + 1, newValue );
-      });
+          next(i + 1, newValue);
+        });
+      }
 
       functions[i].apply(undefined, params);
     }
@@ -29,8 +33,8 @@ function comp(/* functions */){
     hasFirstValue ? next(0, firstValue) : next(0);
   };
 
-  function then(/* [callbacks] */){
-    Array.prototype.push.apply(functions, arguments);
+  function then(fn) {
+    functions.push(fn);
     return call;
   }
 };
--- test.js
+++ test.js
@@ -1,62 +1,48 @@
 var comp = require("./");
 
 describe('comp', function(){
-
   var values, currentStep, step1, step2, step3, errorStep,
       expectedError = new Error('I produce an error');
 
   it('returns a composition of given async functions', function(done){
-
     var steps = comp(step1, step2, step3);
 
     steps(values[0], function(error, value){
-
       expect(error).to.not.exist;
       expect(value).to.equal(values[3]);
 
       done();
-
     });
-
   });
 
   it('stops the progress if a function produced error', function(done){
-
     var steps = comp(step1, step2, errorStep, step3);
 
     steps(values[0], function(error, value){
-
       expect(error).to.equal(expectedError);
-      expect(value).to.equal(values[2]);
+      expect(value).to.not.exist;
       expect(currentStep).to.equal(2.5);
 
       done();
-
     });
-
   });
 
   it('may not take an initial value', function(done){
-
     var steps = comp(step0, step1, step2, step3);
 
     steps(function(error, value){
-
       expect(error).to.not.exist;
       expect(value).to.equal(values[3]);
 
       done();
-
     });
 
     function step0(callback){
       return callback(undefined, values[0]);
     }
-
   });
 
   it('allows adding new functions later with then method', function(done){
-
     var steps = comp(step1);
 
     steps.then(step2).then(step3);
@@ -69,12 +55,22 @@ describe('comp', function(){
       done();
 
     });
-
   });
 
+  it('calls last function with the callback that takes final result', function(done){
+    function foo (v, callback) { callback(undefined, v + 1); }
+    function bar (v, callback) { callback(undefined, v + 2); }
+    function qux (v, callback) { callback(undefined, [v + 3, callback]); }
+
+    comp(foo, bar, qux)(100, function (error, result) {
+      expect(error).to.not.exist;
+      expect(result[0]).to.equal(106);
+      expect(result[1]).to.equal(arguments.callee);
+      done();
+    });
+  });
 
   beforeEach(function(done){
-
     values    = [{ value1: true }, { value2: true }, { value3: true }, { value4: true }];
     step1     = newStep(1);
     step2     = newStep(2);
@@ -82,13 +78,10 @@ describe('comp', function(){
     errorStep = newStep(2.5, expectedError);
 
     done();
-
   });
 
   function newStep(n, error){
-
     return function(value, callback){
-
       currentStep = n;
 
       !error && expect(value).to.equal( values[ n - 1 ] );
@@ -96,8 +89,6 @@ describe('comp', function(){
       process.nextTick(function(){
         callback(error, values[n]);
       });
-
     }
   }
-
 });
